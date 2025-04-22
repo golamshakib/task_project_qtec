d88422d54fe3de685f061a9cbdd3be344241c607
golamshakib <golamshakib.h@gmail.com>
2025-04-22 11:38:28 +0600

    project setup

---
+++ android/app/src/main/kotlin/com/taskproject/task/task_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.taskproject.task.task_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/utils/constants/app_colors.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Brand Colors
+  static const Color primary = Color(0xFFFDCC02);
+  static const Color secondary = Color(0xFFF9F9F9);
+
+
+  // Gradient Colors
+  static const Gradient linearGradient = LinearGradient(
+    begin: Alignment(0.0, 0.0),
+    end: Alignment(0.707, -0.707),
+    colors: [
+      Color(0xfffffa9e),
+      Color(0xFFFAD0C4),
+      Color(0xFFFAD0C4),
+    ],
+  );
+
+
+  // Text Colors
+  static const Color textPrimary = Color(0xFFF9F9F9);
+  static const Color textSecondary = Color(0xFFE8E8E8);
+  static const Color textWhite = Color(0xFFFFFFFF);
+
+
+  // Background Colors
+  static const Color backgroundLight = Color(0xFFF9FAFB);
+  static const Color backgroundDark = Color(0xFF121212);
+  static const Color primaryBackground = Color(0xFFFFFFFF);
+
+  // Surface Colors
+  static const Color surfaceLight = Color(0xFFE0E0E0);
+  static const Color surfaceDark = Color(0xFF2C2C2C);
+
+  // Container Colors
+  static const Color containerBackground = Color(0xFFD9D9D9);
+  static const Color containerBackground1 = Color(0xFFF9F9FB);
+
+  // Utility Colors
+  static const Color success = Color(0xFF4CAF50);
+  static const Color warning = Color(0xFFFFA726);
+  static const Color error = Color(0xFFF44336);
+  static const Color info = Color(0xFF29B6F6);
+  /// textformfield border color
+
+  static const Color textFormFieldBorder = Color(0xFFD9D9D9);
+}
